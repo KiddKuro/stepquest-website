4e1b89685f7dacb762b43c23a5a84a6f5330a264
Adrian Asogu <Adrisogu02@gmail.com>
2026-04-17 18:02:27 +0100

    add env variables

--- stepquest-website/src/StepQuestWebsite.jsx
+++ stepquest-website/src/StepQuestWebsite.jsx
@@ -1,8 +1,10 @@
 import { useState, useRef, useEffect, createContext, useContext } from "react";
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://jngxlfabrccnpmzudvwf.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpuZ3hsZmFicmNjbnBtenVkdndmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ2ODk1MzMsImV4cCI6MjA4MDI2NTUzM30.p_nQaeC2Ffp8zddLC9rEAv1QWPwic7LhamXEEKc44Ew";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY;
+
+
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
